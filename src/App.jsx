5eb17fc674698e296ef5c8f4f7fef0899bacd332
Margarita Flores <mfloreszatarain95@gmail.com>
2022-09-13 11:06:25 -0600

    created styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import styled from "@emotion/styled";
+import ImagenCripto from './img/imagen-criptos.png'
+
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width:90%;
+  @media(min-width: 992px){
+    display:grid;
+    grid-template-columns: repeat(2, 1fr)
+  }
+
+`;
+
+const Imagen = styled.img `
+  max-width: 400px;
+  width: 80%;
+  width: 100px auto 0 auto;
+  display: block;
+`
+
+
+const Heading = styled.h1`
+  font-family: "Lato", sans-serif;
+  color: #b5c9f8;
+  text-align: center;
+  font-weight: 700;
+  margin-top:90px;
+  margin-bottom: 50px;
+  font-size:34px;
+
+  &::after{
+    content: '';
+    width:100px;
+    height: 6px;
+    background-color: #66A2FE;
+    display:block;
+    margin: 10px auto 0 auto;
+  }
+
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+      <Contenedor> 
+        <Imagen
+          src={ImagenCripto}
+          alt="imagen de criptomonedas"
+        />
+            <div> 
+               <Heading> Cotiza Criptomoneadas al Instante</Heading>
+            </div>
+      </Contenedor>
+  
   )
 }
 
-export default App
+export default App;
